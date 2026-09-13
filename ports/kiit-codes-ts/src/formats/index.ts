015ea72b5135e9e161de0c30bc345d5479a99a7e
kishore <kishore.pr4@gmail.com>
2026-09-13 19:47:26 -0400

    add: type to index

---
+++ ports/kiit-codes-ts/src/formats/index.ts
@@ -0,0 +1,5 @@
+export * from "./catalog.js";
+export * from "./error-item.js";
+export * from "./problem.js";
+export * from "./code-detail.js";
+export * from "./codes-to-problem.js";
--- ports/kiit-codes-ts/src/index.ts
+++ ports/kiit-codes-ts/src/index.ts
@@ -4,4 +4,5 @@ export * from "./codes.js";
 export * from "./err.js";
 export * from "./checked.js";
 export * from "./status-error.js";
+export * from "./formats/index.js";
 export { assertNever } from "./assert-never.js";
